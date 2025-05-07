b5956dca0a94d42807e7b502801f1f89db414d0d
SafyaAbouelfadel <safia.abf@gmail.com>
2025-05-07 02:02:12 +0000

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""`stdin` reaader line by line."""
+
+import sys
+
+
+def log(status, file_size):
+    """
+     Log the status and file size.
+
+    Args:
+        status (dict): A dictionary containing status codes and their counts.
+        file_size (int): The size of the file to be logged.
+    """
+    print("File size: {}".format(file_size))
+    for key, val in sorted(status.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
+file_size, counter = 0, 0
+status = dict.fromkeys(
+    ["200", "301", "400", "401", "403", "404", "405", "500"], 0
+)
+
+try:
+    for line in sys.stdin:
+        line = line.split()[::-1]
+        if len(line) > 2:
+            counter += 1
+            if counter <= 10:
+                file_size += int(line[0])
+                if line[1] in status.keys():
+                    status[line[1]] += 1
+            if counter == 10:
+                log(status, file_size)
+                counter = 0
+
+finally:
+    log(status, file_size)
